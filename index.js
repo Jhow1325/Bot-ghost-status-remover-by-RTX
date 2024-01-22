ac2c3f93968ae724a8fdb63ff60f3752e1d52049
Jhow1325 <157413594+Jhow1325@users.noreply.github.com>
2024-01-22 16:16:07 -0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["👑 I am RTX","Spotify","Youtube","SUBSCRIBE❤️","THANKS 🌇"];
+const statusMessages = ["TESTE,"Spotify","TESTE","TESTEE❤️","TESTE 🌇"];
 
 
 let currentIndex = 0;
